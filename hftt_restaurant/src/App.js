f9bf01f2459056f78e2ed94fbd4992d895b684ef
hterreros <harold.terreros.talledo@gmail.com>
2022-07-30 00:57:19 -0500

    Agregando Componentes

--- hftt_restaurant/src/App.js
+++ hftt_restaurant/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
 import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
     <>
+    <NavBar />
     <HomePage />
-      </>
+    <Footer />
+    </>
   )
 }
 
